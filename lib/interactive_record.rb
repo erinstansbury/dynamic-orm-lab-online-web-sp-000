65cdd673c35de4f0ee91762c15e39dd10b088c57
Erin Stansbury <ekstansbury@gmail.com>
2020-06-07 17:18:08 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -60,10 +60,7 @@ class InteractiveRecord
   end
   
   def self.find_by(attribute)
-    sql = <<-SQL
-      SELECT * FROM #{self.table_name} 
-      WHERE #{hash.keys[0].to_s} = '#{hash.values[0].to_s}'
-    SQL
+    sql = "SELECT * FROM #{self.table_name} WHERE #{hash.keys[0].to_s} = '#{hash.values[0].to_s}'"
     DB[:conn].execute(sql)
   end
   
